b2d68386f71036b35f7f4b139fa1c6cbd262729e
NPC <zhazhafei@aliyun.com>
2023-01-07 09:35:02 +0800

    `style: true` 会加载 less 引入ant-design-vue

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).mount('#app')
+createApp(App).use(store).mount('#app')
